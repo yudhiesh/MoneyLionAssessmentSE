b8629bd51a688592c6b27947fe0ebb44c1c8d183
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2021-06-03 23:57:12 +0800

    refactored setting of headers in each request

--- controller/controller.go
+++ controller/controller.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"database/sql"
-	"encoding/json"
 	"errors"
 	"net/http"
 	"unicode/utf8"
@@ -27,31 +26,21 @@ func GetCanAccess(w http.ResponseWriter, r *http.Request) {
 
 	// Check if both url parameters have been set by the user
 	if utf8.RuneCountInString(email) == 0 || utf8.RuneCountInString(featureName) == 0 {
-		responseFailure.Status = http.StatusUnprocessableEntity
-		responseFailure.Message = MissingURLParameter
-		json.NewEncoder(w).Encode(responseFailure)
+		responseFailure.SetHeader(w, MissingURLParameter, http.StatusUnprocessableEntity)
 		return
 	}
 	if err := db.QueryRow(stmt, email, featureName).Scan(&user.CanAccess); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			// Check if the sql query throws a ErrNoRows error
-			responseFailure.Status = http.StatusNotFound
-			responseFailure.Message = NoMatchingRecordFound
-			json.NewEncoder(w).Encode(responseFailure)
-			return
+			responseFailure.SetHeader(w, NoMatchingRecordFound, http.StatusNotFound)
 		} else {
 			// Anything else return a StatusInternalServerError
-			responseFailure.Status = http.StatusInternalServerError
-			responseFailure.Message = Error
-			json.NewEncoder(w).Encode(responseFailure)
-			return
+			responseFailure.SetHeader(w, Error, http.StatusInternalServerError)
 		}
+		return
 	} else {
 		// If successfull return user object
-		responseSuccess.ResponseInfo.Status = http.StatusOK
-		responseSuccess.ResponseInfo.Message = Success
-		responseSuccess.Data = user
-		json.NewEncoder(w).Encode(responseSuccess)
+		responseSuccess.SetHeader(w, Success, http.StatusOK, user)
 	}
 }
 
@@ -66,35 +55,25 @@ func InsertFeature(w http.ResponseWriter, r *http.Request) {
 
 	// Decode body into user struct
 	if err := parse(w, r, &user); err != nil {
-		response.Message = Error
-		response.Status = http.StatusNotModified
-		json.NewEncoder(w).Encode(response)
+		response.SetHeader(w, Error, http.StatusNotModified)
 		return
 	} else {
 		// Validate struct to check if all fields are correct
 		if err := validate.Struct(user); err != nil {
-			response.Message = MissingRequestParameter
-			response.Status = http.StatusNotModified
-			json.NewEncoder(w).Encode(response)
+			response.SetHeader(w, MissingRequestParameter, http.StatusNotModified)
 			return
 		} else {
-			// Check if can_access from the response and the db are different
+			// Check if can_access from the response and the database are different
 			if _, access := CanAccessValue(db, *user.CanAccess, user.Email, user.FeatureName); !access {
-				response.Message = NoMatchingRecordFound
-				response.Status = http.StatusNotModified
-				json.NewEncoder(w).Encode(response)
+				response.SetHeader(w, NoMatchingRecordFound, http.StatusNotModified)
 				return
 
 			} else if _, err = db.Exec(stmt, &user.CanAccess, &user.Email, &user.FeatureName); err != nil {
-				// Execute insert statement to database
-				response.Message = Error
-				response.Status = http.StatusNotModified
-				json.NewEncoder(w).Encode(response)
+				// Update can_access in database
+				response.SetHeader(w, Error, http.StatusNotModified)
 				return
 			} else {
-				response.Message = Success
-				response.Status = http.StatusOK
-				json.NewEncoder(w).Encode(response)
+				response.SetHeader(w, Success, http.StatusOK)
 				return
 			}
 
--- model/model.go
+++ model/model.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type UserCanAccess struct {
 	CanAccess bool `json:"can_access"`
 }
@@ -18,4 +23,19 @@ type ResponseInfo struct {
 type Response struct {
 	ResponseInfo ResponseInfo  `json:"response"`
 	Data         UserCanAccess `json:"data"`
+}
+
+func (r *ResponseInfo) SetHeader(w http.ResponseWriter, message string, status int) {
+	// Set header with status, message and encode it into the response header
+	r.Status = status
+	r.Message = message
+	json.NewEncoder(w).Encode(r)
+}
+
+func (r *Response) SetHeader(w http.ResponseWriter, message string, status int, data UserCanAccess) {
+	// Set header with status, message and encode it into the response header
+	r.ResponseInfo.Status = status
+	r.ResponseInfo.Message = message
+	r.Data = data
+	json.NewEncoder(w).Encode(r)
 }
