09304728333f3cfdf76c6342373f3425bc304ddd
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2021-06-05 20:56:27 +0800

    updated sql script4

--- schema.sql
+++ schema.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE test;
-USE test;
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, 
 	email VARCHAR(255) NOT NULL
 );
@@ -9,13 +7,15 @@ INSERT INTO `users` VALUES (2,"test2@gmail.com");
 INSERT INTO `users` VALUES (3,"test3@gmail.com");
 INSERT INTO `users` VALUES (4,"test4@gmail.com");
 INSERT INTO `users` VALUES (5,"test5@gmail.com");
-CREATE TABLE features (
+
+CREATE TABLE IF NOT EXISTS features (
 	feature_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	user_id INTEGER NOT NULL,
 	feature_name VARCHAR(100) NOT NULL,
 	can_access BOOLEAN NOT NULL DEFAULT FALSE,
 	FOREIGN KEY (user_id) REFERENCES users(id) 
 );
+
 INSERT INTO `features` VALUES (1, 1, "automated-investing", 1);
 INSERT INTO `features` VALUES (2, 1, "crypto", 0);
 INSERT INTO `features` VALUES (3, 2, "crypto", 0);
