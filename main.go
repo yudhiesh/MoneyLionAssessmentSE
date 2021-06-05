bca9b1139427167eb116f39152d9ad617e3237e8
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2021-06-05 15:50:08 +0800

    updated .env for heroku

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
-	"github.com/rs/cors"
 
 	"github.com/yudhiesh/api/controller"
 	"github.com/yudhiesh/api/middleware"
@@ -23,7 +22,9 @@ func main() {
 	db_user := os.Getenv("DB_USER")
 	db_password := os.Getenv("DB_PASSWORD")
 	db_name := os.Getenv("DB_NAME")
-	dsn := db_user + ":" + db_password + "@/" + db_name
+	db_port := os.Getenv("DB_PORT")
+	port := os.Getenv("PORT")
+	dsn := db_user + ":" + db_password + "@tcp(mysql:" + db_port + ")/" + db_name
 	db, err := controller.OpenDB(dsn)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -39,16 +40,9 @@ func main() {
 	router.HandleFunc("/feature", app.GetCanAccess).Methods("GET")
 	router.HandleFunc("/feature", app.InsertFeature).Methods("POST")
 	http.Handle("/", router)
-	port := os.Getenv("PORT")
-	portAddr := ":" + port
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:8000"},
-		AllowCredentials: true,
-	})
-	handler := c.Handler(router)
 	server := &http.Server{
-		Handler: handler,
-		Addr:    portAddr,
+		Handler: router,
+		Addr:    port,
 		// keep-alives last a minute instead of 3 minutes
 		IdleTimeout: time.Minute,
 		// Short ReadTimeout prevents SLowloris attacks
